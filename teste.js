38c5b01c5bb22fa09263556326f6e0babb8ddb93
Luan <luanfkm@gmail.com>
2020-12-17 08:25:13 -0300

    estrutura inicial

--- teste.js
+++ teste.js
@@ -1,6 +1,13 @@
 
-function validarNome(nome) {
+function validar() {
+    let nome = document.getElementById("txt_nome").value;
+    let idade = document.getElementById("txt_idade").value;
+    console.log(nome);
+    console.log(idade);
+}
 
+
+function validarNome(nome) {
 }
 
 function validarIdade(idade) {
