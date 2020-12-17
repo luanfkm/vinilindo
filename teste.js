3d08cedb71e47d637f67a553dd792eb21a0d4d29
vinicius <vinidesenvolve@gmail.com>
2020-12-17 09:24:59 -0300

    Comentário validarNome()

--- teste.js
+++ teste.js
@@ -3,7 +3,7 @@ function validar() {
     let nome = document.getElementById("txt_nome").value;
     let idade = document.getElementById("txt_idade").value;
     validarIdade(idade); //comentario teste
-    validarNome(nome);
+    validarNome(nome); //some daqui mel
     console.log(nome);
     console.log(idade);
 }
